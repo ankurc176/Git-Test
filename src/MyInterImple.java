913f59b27fe694d706acdf55c8f59b56b1fd6d2f
Himmat <ankurc176@gmail.com>
2022-05-02 12:55:09 +0530

    third commit

--- src/MyInterImple.java
+++ src/MyInterImple.java
@@ -5,7 +5,7 @@ public class MyInterImple implements MyInter{
 	public void SayHello() {
 		// TODO Auto-generated method stub
 		System.out.println("Hello My World");
-		
+		System.out.println("This Himmat Kumar");
 	}
 
 }
