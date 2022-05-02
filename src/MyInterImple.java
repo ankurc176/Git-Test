137a1d1b219d1558eff09957560b601dde4ce85a
Himmat <ankurc176@gmail.com>
2022-05-02 19:03:15 +0530

    branch login commit

--- src/MyInterImple.java
+++ src/MyInterImple.java
@@ -6,6 +6,7 @@ public class MyInterImple implements MyInter{
 		// TODO Auto-generated method stub
 		System.out.println("Hello My World");
 		System.out.println("This Himmat Kumar");
+		System.out.println("This is Git test");
 	}
 
 }
