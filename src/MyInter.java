628cdedaf16a5c8eff5e604a75185b94a76a3f6e
Himmat <ankurc176@gmail.com>
2022-05-02 12:24:23 +0530

    first commit

---
+++ src/MyInter.java
@@ -0,0 +1,13 @@
+
+@FunctionalInterface
+public interface MyInter {
+	
+	public void SayHello();
+	
+}
+
+//Three methods to implement Functional Interface 
+
+// create a class and implement an interface.
+// use anonymous class to implement directly on main method.
+// use Lambda expression to implement interface.
---
+++ src/MySumInter.java
@@ -0,0 +1,6 @@
+
+public interface MySumInter {
+	
+	public abstract int sum (int a, int b);
+
+}
